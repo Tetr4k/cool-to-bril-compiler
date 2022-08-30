eb77d832c773a5b6f840221b07980d919653261f
Tetr4k <g2012ribeiro@gmail.com>
2022-08-30 10:22:31 -0300

    Create Cool coding input

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import { useState } from "react";
 function App(){
 	const [tokens, setTokens] = useState([]);
 	const [coolCode, setCoolCode] = useState("");
+	const handleCoolCodeChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setCoolCode(event.target.value);
+	}
+
 	return (
-		<div/>
+		<>
+			<main>
+				<label>
+					Cool:
+					<textarea id="coolCode" value={coolCode} onChange={handleCoolCodeChange}></textarea>
+				</label>
+			</main>
+		</>
 	)
 }
 
